1d57f46e508189fff97724b797385c13caf4d8cc
Haven Lin <v-halin@expedia.com>
2020-09-09 19:12:38 +0800

    Leet #103 BinaryTreeZigzagLevelOrderTraversal

--- src/main/leetcode/editor/cn/BalancedBinaryTree.java
+++ src/main/leetcode/editor/cn/BalancedBinaryTree.java
@@ -39,14 +39,11 @@
 
 package leetcode.editor.cn;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class BalancedBinaryTree{
     public static void main(String[] args) {
         Solution solution =  new BalancedBinaryTree().new Solution();
         String input = "[1,2,2,3,null,null,3,4,null,null,4]";
-        TreeNode treeNode = solution.stringToTreeNode(input);
+        TreeNode treeNode = TreeNodeUtil.stringToTreeNode(input);
         System.out.println(solution.isBalanced(treeNode));
     }
     
@@ -83,50 +80,6 @@ class Solution {
         }
 
         return -1;
-    }
-
-    public TreeNode stringToTreeNode(String input) {
-        input = input.trim();
-        input = input.substring(1, input.length() - 1);
-        if (input.length() == 0) {
-            return null;
-        }
-
-        String[] parts = input.split(",");
-        String item = parts[0];
-        TreeNode root = new TreeNode(Integer.parseInt(item));
-        Queue<TreeNode> nodeQueue = new LinkedList<>();
-        nodeQueue.add(root);
-
-        int index = 1;
-        while (!nodeQueue.isEmpty()) {
-            TreeNode node = nodeQueue.remove();
-
-            if (index == parts.length) {
-                break;
-            }
-
-            item = parts[index++];
-            item = item.trim();
-            if (!item.equals("null")) {
-                int leftNumber = Integer.parseInt(item);
-                node.left = new TreeNode(leftNumber);
-                nodeQueue.add(node.left);
-            }
-
-            if (index == parts.length) {
-                break;
-            }
-
-            item = parts[index++];
-            item = item.trim();
-            if (!item.equals("null")) {
-                int rightNumber = Integer.parseInt(item);
-                node.right = new TreeNode(rightNumber);
-                nodeQueue.add(node.right);
-            }
-        }
-        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
